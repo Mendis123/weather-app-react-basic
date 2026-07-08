f3436b54c450c1f91211e7f77887a7fdbab65c88
Tharindhu Mendis <tharindhu2012@gmail.com>
2026-07-08 12:58:52 -0400

    Added about component render part into the react routing section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import Navbar from "./components/common/Navbar.jsx";
 import { Route, Routes } from "react-router";
 import Home from "./components/home/Home.jsx";
+import About from "./components/about/About.jsx";
 
 const App = () => {
 
   return (
-      <div className="min-h-screen bg-linear-to-br from-blue-500 to-indigo-700">
-        <Navbar />
+    <div className="min-h-screen bg-linear-to-br from-blue-500 to-indigo-700">
+      <Navbar />
 
-        <Routes>
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </div>
-  )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App
