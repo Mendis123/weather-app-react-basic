76f250f8cc1e0e912a3fa011c71fafe3a56efa33
Tharindhu Mendis <tharindhu2012@gmail.com>
2026-07-08 13:45:00 -0400

    Added dummy weather data.

---
+++ src/data/weather.js
@@ -0,0 +1,20 @@
+export const WEATHER_DATA = {
+   anuradhapura: {
+       city: "Anuradhapura",
+       temperature: 31,
+       humidity: 78,
+       wind: 14
+   },
+    colombo: {
+        city: "Colombo",
+        temperature: 32,
+        humidity: 79,
+        wind: 15
+    },
+    jaffna: {
+        city: "Jaffna",
+        temperature: 30,
+        humidity: 76,
+        wind: 12
+    },
+};
